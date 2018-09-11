3bd3c992acbf40c5424e109b733ecec618a85f4e
poyuKiryl <poyuki9@gmail.com>
2018-09-11 10:32:10 +0300

    love-triangle task

--- src/index.js
+++ src/index.js
@@ -3,5 +3,15 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+    let trianglesCount=0,firstLove,secondLove,thirdLove;
+
+    for(let i=0;i<preferences.length;i++){
+        firstLove=preferences[i]-1;
+        secondLove=preferences[preferences[i]-1]-1;
+        thirdLove=preferences[preferences[preferences[i]-1]-1]-1;
+
+        if(thirdLove===i&&firstLove!==i&&secondLove!==firstLove&&thirdLove!==secondLove) trianglesCount++;
+    }
+
+    return trianglesCount/3;
 };
